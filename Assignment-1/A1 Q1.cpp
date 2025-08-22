47454af7ce52d529eba416028326e361541dc3e9
SamarthBadola <samarthbadola@gmail.com>
2025-08-22 13:18:51 +0530

    Create A1 Q1.cpp

---
+++ Assignment-1/A1 Q1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"hello world"<<endl;
+cout<<"hello world with newline operator\n";
+cout<<"hello world with tab operator\t";
+cout<<"hello world with alarm\a"<<endl;
+cout<<"hello world with carriage return\r"; 
+cout<<"a line changed due to carriage return";
+}
